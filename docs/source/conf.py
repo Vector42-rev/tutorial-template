903fb7ae9eb8790936d4fc2ec705721e06cdff9f
Vector42-rev <130288363+Vector42-rev@users.noreply.github.com>
2024-06-14 08:59:39 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Vivado tutorial'
+copyright = ' :)'
+author = 'Vector'
 
 release = '0.1'
 version = '0.1.0'
